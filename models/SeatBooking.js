b0ebdc2c2e41a35b2ad865d5965d4d96690b75e9
Jananga Hasitha <jananga1999@gmail.com>
2022-05-13 17:48:54 +0530

    Ticket booking conditions checking

--- models/SeatBooking.js
+++ models/SeatBooking.js
@@ -12,10 +12,15 @@ class SeatBooking{
         return new Promise(async (resolve, reject)=>{
             try{
                 let [rows, cols] = await db.query(
-                    'UPDATE Seat_Booking SET customer_id=?, booking_date=?, state=? WHERE booking_id=?',
-                    [customer_id, new Date().toISOString(), "occupied", this.booking_id]
+                    'UPDATE Seat_Booking SET customer_id=?, booking_date=?, state=? WHERE booking_id=? and state=?',
+                    [customer_id, new Date().toISOString(), "occupied", this.booking_id, "available"]
                 )
-                resolve(rows)
+                console.log(rows.affectedRows)
+                if(rows.affectedRows){
+                    resolve(true)
+                }else{
+                    resolve(false)
+                }
             } catch(e){return reject(e)}            
         })
     }
