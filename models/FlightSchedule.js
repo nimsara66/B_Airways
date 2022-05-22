7f3742add696cc308a8e07502c0df700b29af63f
Malitjh-19 <malith.19@cse.mrt.ac.lk>
2022-05-22 11:54:41 +0530

    flight schedules set to show dynamically

--- models/FlightSchedule.js
+++ models/FlightSchedule.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const db = require('../db/connect')
 
 class FlightSchedule{
@@ -22,6 +23,34 @@ class FlightSchedule{
         })        
     }
 
+    static getFullSchedule(){
+        // today_date = Date.now();
+        // query_string = "select * from Flight_Schedule where departure_date > "+String(today_date);
+        // fullSchedule = db.query(query_string);
+        // return fullSchedule;
+        return new Promise(async (resolve, reject)=>{
+            try{
+                const today_date = Date.now();
+                let [rows, cols] = await db.query(
+                    `SELECT origin_l.location_id as origin,destination_l.location_id as destination, f.departure_date as date,f.departure_time as time FROM flight_schedule as f 
+                    left outer join route as r on f.route_id = r.route_id
+                    left outer join airport as origin_a on origin_a.airport_id = r.origin
+                    left outer join airport as destination_a on destination_a.airport_id = r.destination
+                    left outer join location as origin_l on origin_a.location_id = origin_l.location_id
+                    left outer join location as destination_l on destination_a.location_id = destination_l.location_id
+                    WHERE f.departure_date>=?`,
+                    // [today_date]
+                    ['2022-05-10']
+                
+                )
+                if(rows.length){
+                    return resolve(rows)
+                }else{
+                    return resolve(false)
+                }
+            } catch(e){return reject(e)}
+        })       
+    }
   
 }
 
--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+const { 
+    home
+} = require('../controllers/scheduleController')
 
-router.get('/', function (req, res, next) {
-    let user=false;
-    res.render('schedule' , { title: 'Express' ,user:user});
-})
+router.get('/',home)
 
 module.exports = router;
