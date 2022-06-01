e0ebb9ed0ec7c0780420b9fc3d1a8bb0f374a4f9
nipun-thejan <thejanfonseka@gmail.com>
2022-06-01 20:13:20 +0530

    Update aircraftmodelController.js

--- controllers/aircraftmodelController.js
+++ controllers/aircraftmodelController.js
@@ -4,8 +4,8 @@ const viewAircraftModels =async (req,res,next) => {
     let user=false;
 
     const aircraftModels =await AircraftModel.getAll();
-    // console.log(schedules);
-    res.render('aircraftModel' , { title: 'Express' ,user:user,aircraftModels});
+    console.log(aircraftModels);
+    res.render('/staff/edit_airplanes' , { title: 'Express' ,user:user,aircraftModels});
 }
 
 
