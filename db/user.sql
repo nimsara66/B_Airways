5f55a9a691f4bf228891dbf4fcaf9e925916b192
Nimsara Fernando <nimsara66@gmail.com>
2022-07-15 21:44:28 +0530

    Give priviledge to missing table

--- db/user.sql
+++ db/user.sql
@@ -5,4 +5,5 @@ GRANT UPDATE, INSERT on B_Airways.Registered_Customer TO 'b_airways_server'@'loc
 GRANT UPDATE, INSERT on B_Airways.Seat_Booking TO 'b_airways_server'@'localhost';
 GRANT UPDATE, INSERT on B_Airways.Guest_Customer TO 'b_airways_server'@'localhost';
 GRANT UPDATE, INSERT on B_Airways.Flight_Schedule TO 'b_airways_server'@'localhost';
+GRANT ALL on B_Airways.sessions TO 'b_airways_server'@'localhost';
 FLUSH PRIVILEGES;
