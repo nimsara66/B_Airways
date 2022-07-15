c03763dc4861124d6def26c98793a1a0ede5fd4e
nipun-thejan <thejanfonseka@gmail.com>
2022-07-15 08:16:39 +0530

    add function
    get_datetime and get_arrival

--- db/dml_sample4.sql
+++ db/dml_sample4.sql
@@ -64,3 +64,36 @@ INSERT INTO Route VALUES
 (63,9, 7, '03:25'),
 (64,9, 10, '02:20');
 
+DROP FUNCTION IF EXISTS get_datetime;
+DELIMITER $$
+
+CREATE FUNCTION get_datetime(val_date DATE, val_time TIME)
+RETURNS datetime
+BEGIN
+	DECLARE val_datetime datetime;
+  	set val_datetime=concat(val_date,' ',val_time);
+END $$
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS get_arrival;
+DELIMITER $$
+
+CREATE FUNCTION get_arrival(
+    val_route_id int(10),
+    val_departure_datetime datetime
+    )
+RETURNS datetime
+BEGIN
+    --DECLARE val_duration time;
+    DECLARE route_duration varchar(10);
+    DECLARE val_arrival_datetime datetime;
+    
+  	SELECT duration INTO route_duration
+  	FROM route WHERE route_id=val_route_id;
+  
+    --set val_duration = STR_TO_DATE(d,'%h%i%s');
+
+  	set val_arrival_datetime = ADDTIME(val_departure_datetime, route_duration);
+  	RETURN val_arrival_datetime;
+END $$
+DELIMITER ;
