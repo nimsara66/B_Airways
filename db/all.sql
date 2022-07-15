a92b347c1455504efb2755a183c22c7e95e1cacc
Nimsara Fernando <nimsara66@gmail.com>
2022-07-16 00:26:58 +0530

    Resolve conflict in all.sql

--- db/all.sql
+++ db/all.sql
@@ -1,4 +1,12 @@
 CREATE USER 'b_airways_server'@'localhost' IDENTIFIED BY '123456';
+-- GRANT SELECT on B_Airways.* TO 'b_airways_server'@'localhost';
+-- GRANT UPDATE, INSERT on B_Airways.Customer TO 'b_airways_server'@'localhost';
+-- GRANT UPDATE, INSERT on B_Airways.Registered_Customer TO 'b_airways_server'@'localhost';
+-- GRANT UPDATE, INSERT on B_Airways.Seat_Booking TO 'b_airways_server'@'localhost';
+-- GRANT UPDATE, INSERT on B_Airways.Guest_Customer TO 'b_airways_server'@'localhost';
+-- GRANT UPDATE, INSERT on B_Airways.Flight_Schedule TO 'b_airways_server'@'localhost';
+-- GRANT ALL on B_Airways.sessions TO 'b_airways_server'@'localhost';
+-- FLUSH PRIVILEGES;
 
 create table Traveller_Class(
     traveller_class_id      int(10) primary key,
