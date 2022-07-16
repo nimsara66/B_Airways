c54ab132b765bbdba55134755dae3f93bc4e37de
Jananga Kalawana <jananga1999@gmail.com>
2022-07-16 09:52:38 +0530

    Report 1

--- db/report1.sql
+++ db/report1.sql
@@ -52,12 +52,58 @@ BEGIN
 END$$
 DELIMITER ;
 
-SELECT count(*) FROM Seat_Booking WHERE schedule_id=
-(SELECT schedule_id FROM Flight_Schedule WHERE departure_date>CURDATE() OR departure_date=CURDATE() AND departure_time>=CURRENT_TIME() ORDER
-    BY departure_date, departure_time LIMIT 1) AND state='occupied'
+DROP FUNCTION IF EXISTS GetCountGreater18;
+DELIMITER $$
+CREATE FUNCTION GetCountGreater18(aircraft_id_in int(10))
+RETURNS DOUBLE
+BEGIN
+    DECLARE passenger_count_out DOUBLE;
+    SELECT count(*) INTO passenger_count_out FROM Seat_Booking WHERE schedule_id=
+(SELECT schedule_id FROM Flight_Schedule WHERE aircraft_id=aircraft_id_in AND departure_date>CURDATE() OR departure_date=CURDATE() AND departure_time>=CURRENT_TIME() 
+    ORDER BY departure_date, departure_time LIMIT 1) AND state='occupied'
     AND GetCustomerAge(customer_id)>18;
+    RETURN passenger_count_out;
+END$$
+DELIMITER ;
 
-SELECT count(*) FROM Seat_Booking WHERE schedule_id=
-(SELECT schedule_id FROM Flight_Schedule WHERE departure_date>CURDATE() OR departure_date=CURDATE() AND departure_time>=CURRENT_TIME() ORDER
+DROP FUNCTION IF EXISTS GetCountBelow18;
+DELIMITER $$
+CREATE FUNCTION GetCountBelow18(aircraft_id_in int(10))
+RETURNS DOUBLE
+BEGIN
+    DECLARE passenger_count_out DOUBLE;
+    SELECT count(*) INTO passenger_count_out FROM Seat_Booking WHERE schedule_id=
+(SELECT schedule_id FROM Flight_Schedule WHERE aircraft_id=aircraft_id_in AND departure_date>CURDATE() OR departure_date=CURDATE() AND departure_time>=CURRENT_TIME() ORDER
     BY departure_date, departure_time LIMIT 1) AND state='occupied'
     AND GetCustomerAge(customer_id)<18;
+    RETURN passenger_count_out;
+END$$
+DELIMITER ;
+
+
+
+
+SELECT GetCountGreater18(1);
+SELECT GetCountBelow18(1);
+
+/*   Testing  */
+CALL insert_flight_schedule(1,1,'2022-07-17','12:00:00', 0);
+
+CALL SeatBook(87,1,3,@isBookingSuccess);
+CALL SeatBook(87,2,4,@isBookingSuccess);
+CALL SeatBook(87,3,5,@isBookingSuccess);
+CALL SeatBook(87,4,6,@isBookingSuccess);
+CALL SeatBook(87,5,7,@isBookingSuccess);
+
+CALL SeatBook(87,6,8,@isBookingSuccess);
+CALL SeatBook(87,7,9,@isBookingSuccess);
+CALL SeatBook(87,8,10,@isBookingSuccess);
+CALL SeatBook(87,9,11,@isBookingSuccess);
+CALL SeatBook(87,10,12,@isBookingSuccess);
+CALL SeatBook(87,11,13,@isBookingSuccess);
+CALL SeatBook(87,12,14,@isBookingSuccess);
+CALL SeatBook(87,13,15,@isBookingSuccess);
+CALL SeatBook(87,14,16,@isBookingSuccess);
+CALL SeatBook(87,15,7,@isBookingSuccess);
+
+
