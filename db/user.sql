917fe221033aea3a3f7edccb8c99ecbc5c1fe3c0
Nimsara Fernando <nimsara66@gmail.com>
2022-07-16 08:09:35 +0530

    Update user.sql to work properly

--- db/user.sql
+++ db/user.sql
@@ -1,9 +1,12 @@
 CREATE USER 'b_airways_server'@'localhost' IDENTIFIED BY '123456';
-GRANT SELECT on B_Airways.* TO 'b_airways_server'@'localhost';
-GRANT UPDATE, INSERT on B_Airways.Customer TO 'b_airways_server'@'localhost';
-GRANT UPDATE, INSERT on B_Airways.Registered_Customer TO 'b_airways_server'@'localhost';
-GRANT UPDATE, INSERT on B_Airways.Seat_Booking TO 'b_airways_server'@'localhost';
-GRANT UPDATE, INSERT on B_Airways.Guest_Customer TO 'b_airways_server'@'localhost';
-GRANT UPDATE, INSERT on B_Airways.Flight_Schedule TO 'b_airways_server'@'localhost';
+GRANT SELECT, UPDATE, INSERT on B_Airways.* TO 'b_airways_server'@'localhost';
+GRANT EXECUTE ON B_Airways.* TO 'b_airways_server'@'localhost';
 GRANT ALL on B_Airways.sessions TO 'b_airways_server'@'localhost';
 FLUSH PRIVILEGES;
+
+
+-- GRANT UPDATE, INSERT on B_Airways.Customer TO 'b_airways_server'@'localhost';
+-- GRANT UPDATE, INSERT on B_Airways.Registered_Customer TO 'b_airways_server'@'localhost';
+-- GRANT UPDATE, INSERT on B_Airways.Seat_Booking TO 'b_airways_server'@'localhost';
+-- GRANT UPDATE, INSERT on B_Airways.Guest_Customer TO 'b_airways_server'@'localhost';
+-- GRANT UPDATE, INSERT on B_Airways.Flight_Schedule TO 'b_airways_server'@'localhost';
