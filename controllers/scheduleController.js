1cca70936e33bf0cee27ef4e37b865b3f3e88727
sanduninduwara <sanduninduwara2@gmail.com>
2022-07-16 09:19:02 +0530

    fix bugs

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,55 +1,55 @@
 const FlightSchedule = require("../models/FlightSchedule")
 const Aircraft = require("../models/Aircraft")
 
-const home =async (req,res,next) => {
-    let user=false;
+const home = async (req, res, next) => {
+    let user = req.user;
 
-    const schedules =await FlightSchedule.getFullSchedule();
+    const schedules = await FlightSchedule.getFullSchedule();
     // console.log(schedules);
-    res.render('schedule' , { title: 'Express' ,user:user,schedules});
+    res.render('schedule', { title: 'Express', user: user, schedules });
 }
 
 
-const getCreate = async (req,res,next)=>{
+const getCreate = async (req, res, next) => {
     let user = req.user;
     let msg = req.session.msg;
     let route_id = req.params.route_id;
-    try{
+    try {
         let aircrafts = await Aircraft.getAllAircrafts()
-        res.render('schedule/create', {user,msg, route_id, aircrafts})
-    }catch(err){
-        if(err){
+        res.render('schedule/create', { user, msg, route_id, aircrafts })
+    } catch (err) {
+        if (err) {
             return next(err)
         }
     }
 }
 
 
-const create = async (req,res,next)=>{
+const create = async (req, res, next) => {
     let user = req.user;
-    try{
+    try {
         let aircraft_id = req.body.aircraft_id
         let [result, _] = await FlightSchedule.create({
-            route_id:req.params.route_id,
+            route_id: req.params.route_id,
             aircraft_id,
-            departure_date:req.body.departure_date,
-            departure_time:req.body.departure_time,
-            arrival_date:req.body.departure_date,
-            arrival_time:req.body.departure_time
+            departure_date: req.body.departure_date,
+            departure_time: req.body.departure_time,
+            arrival_date: req.body.departure_date,
+            arrival_time: req.body.departure_time
         })
-        if(result.insertId){
+        if (result.insertId) {
             req.session.msg = "Schedule created successfully."
-        }else{
+        } else {
             req.session.msg = "Schedule creating failed. Please try again."
         }
-        res.redirect('/schedule/create/'+req.params.route_id)
-    }catch(err){
-        if(err) next(err)
+        res.redirect('/schedule/create/' + req.params.route_id)
+    } catch (err) {
+        if (err) next(err)
     }
 }
 
 
-module.exports={
+module.exports = {
     home,
     getCreate,
     create
